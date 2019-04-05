cc8bb43298864bd1d211760bd6e0073ec1400a03
Adam Novak <anovak@soe.ucsc.edu>
2019-04-04 17:06:45 -0700

    Find htslib with pkg-config instead

--- cmake/FindHTSlib.cmake
+++ cmake/FindHTSlib.cmake
@@ -1,15 +1,8 @@
 # Explain how to find HTSlib, since CMake by default does not know what HTSlib is.
-# See <https://github.com/Intel-HLS/GenomicsDB/blob/0ad5451950661900c8e6c9db0c084ebecdf2351f/cmake/Modules/Findhtslib.cmake>
 
-# Get the standard package finder.
-# See https://cmake.org/cmake/help/v3.6/module/FindPackageHandleStandardArgs.html
-include(FindPackageHandleStandardArgs)
+# Do it with pkg-config
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(HTSlib REQUIRED htslib)
 
-# The include directory is wherever has this header under an "htslib" folder.
-find_path(HTSLIB_INCLUDE_DIR htslib/hts.h)
-
-# The library is going to be what we would link with -lhts
-find_library(HTSLIB_LIBRARY hts)
-
-# Say we found the library if we found those things, and fail otherwise.
-find_package_handle_standard_args(HTSlib DEFAULT_MSG HTSLIB_INCLUDE_DIR HTSLIB_LIBRARY)
+# As in https://cmake.org/cmake/help/v3.0/module/FindPkgConfig.html we put our
+# libraries in HTSlib_LIBRARIES and our static libraries in HTSlib_STATIC_LIBRARIES.
