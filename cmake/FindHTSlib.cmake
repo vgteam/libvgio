d493e745b348c9564dda45fbda2cbf88e344d5ea
Adam Novak <anovak@soe.ucsc.edu>
2019-04-04 17:49:16 -0700

    Find shared and static dependencies with pkgconfig

--- cmake/FindHTSlib.cmake
+++
@@ -1,8 +0,0 @@
-# Explain how to find HTSlib, since CMake by default does not know what HTSlib is.
-
-# Do it with pkg-config
-find_package(PkgConfig REQUIRED)
-pkg_check_modules(HTSlib REQUIRED htslib)
-
-# As in https://cmake.org/cmake/help/v3.0/module/FindPkgConfig.html we put our
-# libraries in HTSlib_LIBRARIES and our static libraries in HTSlib_STATIC_LIBRARIES.
