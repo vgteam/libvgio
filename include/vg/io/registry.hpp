11dd8e92166ac71bf0e1979b9a2c378883e978be
Glenn Hickey <glenn.hickey@gmail.com>
2021-03-24 20:01:41 -0400

    optionally remember filename istreams come in from

--- include/vg/io/registry.hpp
+++ include/vg/io/registry.hpp
@@ -53,6 +53,9 @@ using save_function_t = function<void(const void*, const message_consumer_functi
 /// This is the type of a function that can load an object of unspecified type from a bare input stream.
 using bare_load_function_t = function<void*(istream&)>;
 
+/// Like above, but keep track of an optional (can be empty) filename (ie where stream comes from)
+using bare_load_function_with_filename_t = function<void*(istream&, const string&)>;
+
 /// This is the type of a function that can save an object of unspecified type to a bare output stream.
 using bare_save_function_t = function<void(const void*, ostream&)>;
 
@@ -163,8 +166,8 @@ public:
      */
     template<typename Handled, typename... Bases>
     static void register_bare_loader_saver_with_header_check(const string& tag,
-        function<bool(istream&)> check_header,
-        bare_load_function_t loader, bare_save_function_t saver);    
+        function<bool(istream&)> sniff_header,
+        bare_load_function_with_filename_t loader, bare_save_function_t saver);    
         
     /**
      * Register a load function for a tag. The empty tag means it can run on
@@ -242,7 +245,7 @@ public:
      * functions available, returns nullptr.
      */
     template<typename Want>
-    static const vector<pair<bare_load_function_t, function<bool(istream&)>>>* find_bare_loaders();
+    static const vector<pair<bare_load_function_with_filename_t, function<bool(istream&)>>>* find_bare_loaders();
     
     /**
      * Look up the appropriate saver function to use to save an object of the
@@ -273,7 +276,7 @@ private:
         /// Maps from type_index we want to load from a old,
         /// non-tagged-message-format file to a list of "bare" loaders that can load the
         /// desired thing from an istream, and their possibly empty required-prefix-sniffer-functions.
-        unordered_map<type_index, vector<pair<bare_load_function_t, function<bool(istream&)>>>> type_to_bare_loaders;
+        unordered_map<type_index, vector<pair<bare_load_function_with_filename_t, function<bool(istream&)>>>> type_to_bare_loaders;
     };
     
     /**
@@ -295,6 +298,14 @@ private:
     template<typename Handled, typename... Bases>
     static void register_bare_loader(bare_load_function_t loader,
                                      function<bool(istream&)> sniff_header = nullptr);
+
+    /**
+     * Slight generalization of above that allows filename to be passed alongside the
+     * input stream.
+     */
+    template<typename Handled, typename... Bases>
+    static void register_bare_loader_with_filename(bare_load_function_with_filename_t loader,
+                                                   function<bool(istream&)> sniff_header = nullptr);
     
     /**
      * Register a save function to save a type with a given tag. The empty tag
@@ -353,16 +364,26 @@ void Registry::register_loader(const std::vector<std::string>& tags, load_functi
 }
 
 template<typename Handled, typename... Bases>
-void Registry::register_bare_loader(bare_load_function_t loader, function<bool(istream&)> check_header) {
+void Registry::register_bare_loader(bare_load_function_t loader, function<bool(istream&)> sniff_header) {
+    // I'm sorry
+    bare_load_function_with_filename_t named_loader = [loader](istream& stream, const string&filename) {
+            return loader(stream);
+    };
+
+    register_bare_loader_with_filename<Handled, Bases...>(named_loader, sniff_header);
+}
+    
+template<typename Handled, typename... Bases>
+void Registry::register_bare_loader_with_filename(bare_load_function_with_filename_t loader, function<bool(istream&)> sniff_header) {
     // Get our state
     Tables& tables = get_tables();
     
     // Save the loading function to load the given type
-    tables.type_to_bare_loaders[type_index(typeid(Handled))].emplace_back(loader, check_header);
+    tables.type_to_bare_loaders[type_index(typeid(Handled))].emplace_back(loader, sniff_header);
 
     // And for the base classes.
     // Expand over all the base types in an initializer list and use an assignment expression to fill a dummy vector.
-    std::vector<int> dummy{(tables.type_to_bare_loaders[type_index(typeid(Bases))].emplace_back(loader, check_header), 0)...};
+    std::vector<int> dummy{(tables.type_to_bare_loaders[type_index(typeid(Bases))].emplace_back(loader, sniff_header), 0)...};
 }
 
 template<typename Handled>
@@ -445,14 +466,19 @@ void Registry::register_bare_loader_saver_with_magics(const string& tag, const v
 }
 
 template<typename Handled, typename... Bases>
-void Registry::register_bare_loader_saver_with_header_check(const string& tag, function<bool(istream&)> check_header,
-    bare_load_function_t loader, bare_save_function_t saver) {
+void Registry::register_bare_loader_saver_with_header_check(const string& tag, function<bool(istream&)> sniff_header,
+    bare_load_function_with_filename_t loader, bare_save_function_t saver) {
 
+    // I'm sorry
+    bare_load_function_t bare_loader = [loader](istream& stream) {
+        return loader(stream, "");
+    };
+    
     // Register the type-tagged wrapped functions
-    register_loader_saver<Handled, Bases...>(tag, wrap_bare_loader(loader), wrap_bare_saver(saver));
+    register_loader_saver<Handled, Bases...>(tag, wrap_bare_loader(bare_loader), wrap_bare_saver(saver));
 
     // Register the bare stream loader with header check
-    register_bare_loader<Handled, Bases...>(loader, check_header);
+    register_bare_loader_with_filename<Handled, Bases...>(loader, sniff_header);
 }
 
 template<typename Want>
@@ -485,7 +511,7 @@ const load_function_t* Registry::find_loader(const string& tag) {
 }
 
 template<typename Want>
-const vector<pair<bare_load_function_t, function<bool(istream&)>>>* Registry::find_bare_loaders() {
+const vector<pair<bare_load_function_with_filename_t, function<bool(istream&)>>>* Registry::find_bare_loaders() {
     // Get our state
     Tables& tables = get_tables();
     
--- include/vg/io/vpkg.hpp
+++ include/vg/io/vpkg.hpp
@@ -125,9 +125,11 @@ public:
      * The stream may be VPKG with the appropriate tag, or a bare non-VPKG stream understood by the loader.
      * Tagged messages that can't be used to load the thing we are looking for are skipped.
      * This doesn't work for Protobuf messages directly, but it will work for e.g. vg::VG.
+     * filename is optional, and can be used by callback code that may, for some reason,
+     * want to link the stream back to a path (cough cough GFA loader cough cough)
      */
     template<typename Wanted>
-    static unique_ptr<Wanted> try_load_one(istream& in) {
+    static unique_ptr<Wanted> try_load_one(istream& in, const string& filename = "") {
         
         istream* from_ptr = &in;
         unique_ptr<streamistream> wrapper;
@@ -206,7 +208,7 @@ public:
                         if (check_header_fn != nullptr && check_header_fn(from)) {
                             // Use the first prefix-match we find.
                             // Up to the user to avoid prefix overlap.
-                            return unique_ptr<Wanted>((Wanted*)(loader_and_prefix.first)(from));
+                            return unique_ptr<Wanted>((Wanted*)(loader_and_prefix.first)(from, filename));
                         }
                         // todo: do we need to do something if check_header_fn is null? 
                     }
@@ -297,7 +299,7 @@ public:
             ifstream open_file(filename.c_str());
             
             // Read from it
-            return try_load_one<Wanted>(open_file);
+            return try_load_one<Wanted>(open_file, filename);
         }
     }
     
@@ -310,14 +312,14 @@ public:
      * May consume trailing data from the stream.
      */
     template<typename Wanted>
-    static unique_ptr<Wanted> load_one(istream& in) {
+    static unique_ptr<Wanted> load_one(istream& in, const string& filename = "") {
         if (!in) {
             cerr << "error[VPKG::load_one]: Unreadable stream while loading " << describe<Wanted>() << endl;
             exit(1);
         }
 
         // Read from it
-        auto result = try_load_one<Wanted>(in);
+        auto result = try_load_one<Wanted>(in, filename);
         
         if (result.get() == nullptr) {
             cerr << "error[VPKG::load_one]: Correct input type not found while loading " << describe<Wanted>() << endl;
@@ -370,7 +372,7 @@ public:
             }
             
             // Read the file.
-            auto result = try_load_one<Wanted>(in);
+            auto result = try_load_one<Wanted>(in, filename);
             
             if (result.get() == nullptr) {
                 cerr << "error[VPKG::load_one]: Correct input type not found in " << filename << " while loading " << describe<Wanted>() << endl;
