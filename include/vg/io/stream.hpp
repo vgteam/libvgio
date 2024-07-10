cb4a54c693436b78c015c30057899ead43f61f30
Adam Novak <anovak@soe.ucsc.edu>
2024-07-09 20:09:39 -0400

    Turn off stream multithreading since it seems broken on Mac

--- include/vg/io/stream.hpp
+++ include/vg/io/stream.hpp
@@ -191,9 +191,8 @@ void for_each_parallel_impl(std::istream& in,
         };
         
         // We do our own multi-threaded Protobuf decoding, but we batch up our
-        // strings by pulling them from this iterator, which we also
-        // multi-thread for decompression.
-        MessageIterator message_it(in, false, 8);
+        // strings by pulling them from this iterator.
+        MessageIterator message_it(in, false);
 
         std::vector<std::string> *batch = nullptr;
         
